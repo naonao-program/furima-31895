4a38c57b91e9cf4304bd0a75aeefcfd1a54dbe6b
naonao-program <gf19a012@oecu.jp>
2021-01-30 16:03:39 +0900

    テストコードの記述 Faker FactoryBotの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,13 @@ class User < ApplicationRecord
   has_many :item
   has_many :order
 
-  validates :nickname, presence: true
-  with_options format: {with: /\A[ぁ-んァ-ン一-龥]/} do
-    validates :last_name
-    validates :first_name
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :password,        format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])/}, length: { minimum: 6 }
+    validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :last_name_kana,  format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+    validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
   end
-  with_options format: {with: /\A[ァ-ヶー－]+\z/} do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
-  validates :birthday, presence: true
 end
