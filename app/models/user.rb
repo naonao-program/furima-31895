255b3d4a0b9f3afe171fc665c3a80d512677cdc3
naonao-program <gf19a012@oecu.jp>
2021-01-30 16:10:02 +0900

    Rubocopで自動整形を実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :item
   has_many :order
 
   with_options presence: true do
     validates :nickname
     validates :birthday
-    validates :password,        format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])/}, length: { minimum: 6 }
-    validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-    validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-    validates :last_name_kana,  format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-    validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+    validates :password,        format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)/ }, length: { minimum: 6 }
+    validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :last_name_kana,  format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
   end
 end
