afddb7192adf068944320ac1e473fe808228bb04
naonao-program <gf19a012@oecu.jp>
2021-02-02 16:11:39 +0900

    itemsコントローラーへnewアクションとcreateアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :postage_id, :region_id, :shipping_date_id, :price, :user_id)
+  end
 end
