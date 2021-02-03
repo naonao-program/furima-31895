e9bd7b7c8e2a5228255a77befd97ac1b2a6b53ff
naonao-program <gf19a012@oecu.jp>
2021-02-03 16:16:28 +0900

    出品機能を実装完了した

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :item
+  has_many :items
   has_many :order
 
   with_options presence: true do
