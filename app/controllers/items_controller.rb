d2fbe7ab2bd9e22ba9ec3077a7a230c9c472299f
naonao-program <gf19a012@oecu.jp>
2021-02-09 14:13:26 +0900

    rubocopを実行してコードを自動編集した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,8 +52,6 @@ class ItemsController < ApplicationController
   end
 
   def redirect_root_path
-    unless current_user.id == @item.user.id
-      redirect_to root_path
-    end
+    redirect_to root_path unless current_user.id == @item.user.id
   end
 end
