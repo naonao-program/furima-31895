656c79fd211c9d03222691ad431c8e508edb8f20
naonao-program <gf19a012@oecu.jp>
2021-02-12 22:45:20 +0900

    indexアクションの中身と createアクションを記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,16 @@
 class OrdersController < ApplicationController
   def index
+    @order = Order.new
   end
   
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_root_path
+    else
+      render 'index'
+    end
+  end
 end
+
