1dfffa0131a52a288d8a6551d2f6698595555b4e
naonao-program <gf19a012@oecu.jp>
2021-02-13 16:45:35 +0900

    データをテーブルに保存する処理を書いた

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -10,5 +10,9 @@ class Orderform
   validates :address
   validates :phone_number, format: {with: /\A\d{10,11}\z/}
   end
-  
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, region_id: region_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order_id)
+  end
 end
