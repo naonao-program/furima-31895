01ac9b7a764f8b83ba04034fa1b32fc285e5557a
naonao-program <gf19a012@oecu.jp>
2021-02-15 15:41:38 +0900

    Itemのデータを引き出す記述をまとめた&バリデーションを確認してからデータが保存されるようにした

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,13 +5,19 @@ class OrdersController < ApplicationController
   end
   
   def create
-    @order_form = OrderForm.new(order_form_params)
     @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new(order_form_params)
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
   def order_form_params
-    params.require(:order_form).permit(:postal_code, :region_id, :municipality, :address, :building_name, :phone_number, :order_id).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_form).permit(:postal_code, :region_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,4 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :address
-
-  validates :user_id, presence: true
-  validates :item_id, presence: true
 end
